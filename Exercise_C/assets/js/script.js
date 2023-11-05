b83a2f2d3c78184a00a429b29537d9dec50d5fd0
AlexT0019 <ying3438@gmail.com>
2023-11-05 23:43:49 +0800

    completed exercise

--- Exercise_C/assets/js/script.js
+++ Exercise_C/assets/js/script.js
@@ -1,1 +1,9 @@
 // Write your JavaScript script here.
+function myFunction() {
+    var x = document.getElementsByClassName("nav-item");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
